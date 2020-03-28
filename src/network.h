e662f4934654f90ca1e02d9ee42c591e2dc7ce59
Jim Breslin <jbreslin33@gmail.com>
2020-03-28 17:40:10 -0400

    af

---
+++ src/network.h
@@ -0,0 +1,32 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+class Network
+{
+	private:
+
+        int sock;
+        struct sockaddr_in sa;
+        char buffer[1024];
+        ssize_t recsize;
+        socklen_t fromlen;
+
+	public:
+
+	Network();
+
+
+};
+
+#endif
