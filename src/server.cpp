77b136b465ce8f876e74e585f4d993267361982b
Jim Breslin <jbreslin33@gmail.com>
2020-03-31 13:24:31 -0400

    getting move and port back???

--- src/server.cpp
+++ src/server.cpp
@@ -27,19 +27,23 @@ void Server::processData(std::string s, struct sockaddr_in socketAddressIn)
 	if (s.compare(0,1,"1") == 0)
 	{
 		//Move
-		printf ("%s \n", "Move came in.");
+		printf ("Move came in");
 	}
 	if (s.compare(0,1,"2") == 0)
 	{
 
 		//lets grab the port
-		std::string s.
+		std::string port = s.substr(1,5);
 
 		this->mClientIdCounter++;
 		Client* client = new Client(this->mClientIdCounter, &socketAddressIn);
 		mNetwork->mClientVector.push_back(client);
+		
+		printf ("Client: %d listening on port: %s\n", client->getId(), port.c_str());
 
-		//mNetwork->mClientVector.at(
-		client->sendMessageToClient("MethactonBitches");
+		//make message to send to clients
+		std::string message = "Server will send messages to port: ";
+		message.append(port);
+		client->sendMessageToClient(message);
 	}
 }
