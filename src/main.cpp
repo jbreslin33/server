7ce1d5adeb09b4b11d97dc70de4b54a9d5393785
Jim Breslin <jbreslin33@gmail.com>
2020-03-31 20:13:42 -0400

    to server from client

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,45 @@
 #include "relay.h"
 
 
+void writeData(Relay* relay)
+{
+	int sock;
+  	struct sockaddr_in sa;
+  	int bytes_sent;
+  	char buffer[200];
+ 
+  	strcpy(buffer, "hello to client from serve!");
+ 
+  	/* create an Internet, datagram, socket using UDP */
+  	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  	if (sock == -1) 
+	{
+      		/* if socket failed to initialize, exit */
+      		printf("Error Creating Socket");
+      		exit(EXIT_FAILURE);
+  	}
+ 
+  	/* Zero out socket address */
+  	memset(&sa, 0, sizeof sa);
+  
+  	/* The address is IPv4 */
+  	sa.sin_family = AF_INET;
+ 
+   	/* IPv4 adresses is a uint32_t, convert a string representation of the octets to the appropriate value */
+  	sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+  
+  	/* sockets are unsigned shorts, htons(x) ensures x is in network byte order, set the port to 7654 */
+  	sa.sin_port = htons(8765);
+ 
+  	bytes_sent = sendto(sock, buffer, strlen(buffer), 0,(struct sockaddr*)&sa, sizeof sa);
+  	if (bytes_sent < 0) 
+	{
+    		printf("Error sending packet: %s\n", strerror(errno));
+    		exit(EXIT_FAILURE);
+  	}
+ 
+  	close(sock); /* close the socket */
+}
 
 void readSocketData(Relay* relay)
 {
@@ -55,10 +94,10 @@ int main(void)
 	Relay relay;
 
         std::thread socketReader (readSocketData, &relay);
+        std::thread writer       (writeData, &relay);
 
-        //std::thread writer       (writeData, &server);
         socketReader.join();
-        //writer.join();
+        writer.join();
 
         return 0;
 
