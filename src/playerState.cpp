60b3b5d955815117f2d8ab56438ed363d1fe2bdc
Jim Breslin <jbreslin33@gmail.com>
2020-04-12 19:05:52 -0400

    playerStateMachine

---
+++ src/playerState.cpp
@@ -0,0 +1,19 @@
+#include "playerState.h"
+
+PlayerState::PlayerState()
+{
+
+}
+
+void PlayerState::enter(Player* player)
+{
+
+}
+void PlayerState::execute(Player* player)
+{
+
+}
+void PlayerState::exit(Player* player)
+{
+
+}
